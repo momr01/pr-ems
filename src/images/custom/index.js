c2745b3825bbe68f4d20602a3bac25eb17c97445
Maximiliano Montaña <maxi.omr01@gmail.com>
2023-02-03 22:02:51 -0300

    change extension of image in login component

--- src/images/custom/index.js
+++ src/images/custom/index.js
@@ -4,6 +4,8 @@ import EMSLogoIcon from "./ems-logo-removebg-preview.png";
 import UsaFlag from "./usa.png";
 import UserTest from "./user-28-01.jpg";
 
+//export images
+
 export default {
   LoginBg,
   EMSLogo,
